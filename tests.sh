2b8646a847a741fe096483758524cad07bd97c27
Etan Reisner <deryni@unreliablesource.net>
2015-10-18 22:12:02 -0400

    Prep for expandString testing.

--- tests.sh
+++ tests.sh
@@ -13,18 +13,22 @@ strexp() {
 }
 
 t() {
-    exp=$(expandAssign "$1")
+    exp=$("$1" "$2")
     if [ "$v" ]; then
         printf \\n >&2
-        printf 'Original: %s\n' "$1" >&2
+        printf 'Original: %s\n' "$2" >&2
         printf 'Expanded: %s\n' "$exp" >&2
-        printf 'Expected: %s\n' "$2" >&2
+        printf 'Expected: %s\n' "$3" >&2
     fi
-    if [ "$2" = "$exp" ]; then
-        printf 'Succeeded: '\''%s'\''\n' "$1"
+    if [ "$3" = "$exp" ]; then
+        printf 'Succeeded: '\''%s'\''\n' "$2"
     else
-        printf 'Failed: '\''%s'\''\n' "$1"
+        printf 'Failed: '\''%s'\''\n' "$2"
     fi
+}
+
+ta() {
+    t expandAssign "$@"
 }
 
 cd /tmp; cd -
@@ -34,20 +38,20 @@ name2="~crl/Documents/double  spaced"
 name3="/work/whiffle/two  spaces  are  better  than one"
 name4="~testuser/Documents/double  spaced"
 
-t "$name1" "$(strexp "$name1")"
-t "$name2" "$(strexp "$name2")"
-t "$name3" "$(strexp "$name3")"
-t "$name4" "$(strexp "$name4")"
-t "~"          "$HOME"
-t "~/"         "$HOME/"
-t "~crl"       ~crl
-t "~crl/"      ~crl/
-t "~testuser"  ~testuser
-t "~testuser/" ~testuser/
-t "~+"         ~+
-t "~+/plus"    ~+/plus
-t "~-"         ~-
-t "~-/minus"   ~-/minus
+ta "$name1" "$(strexp "$name1")"
+ta "$name2" "$(strexp "$name2")"
+ta "$name3" "$(strexp "$name3")"
+ta "$name4" "$(strexp "$name4")"
+ta "~"          "$HOME"
+ta "~/"         "$HOME/"
+ta "~crl"       ~crl
+ta "~crl/"      ~crl/
+ta "~testuser"  ~testuser
+ta "~testuser/" ~testuser/
+ta "~+"         ~+
+ta "~+/plus"    ~+/plus
+ta "~-"         ~-
+ta "~-/minus"   ~-/minus
 
 o=$PWD
 pushd / >/dev/null
@@ -56,12 +60,12 @@ pushd /opt >/dev/null
 pushd /var >/dev/null
 pushd "$o" >/dev/null
 
-t  '~1'  ~1
-t '~+1' ~+1
-t '~-1' ~-1
-t  '~2'  ~2
-t '~+2' ~+2
-t '~-2' ~-2
-t  '~8'  ~8
-t '~+8' ~+8
-t '~-8' ~-8
+ta  '~1'  ~1
+ta '~+1' ~+1
+ta '~-1' ~-1
+ta  '~2'  ~2
+ta '~+2' ~+2
+ta '~-2' ~-2
+ta  '~8'  ~8
+ta '~+8' ~+8
+ta '~-8' ~-8
