d98726702ff4d8d8c26b38bd1401bfcf0b1f0231
Etan Reisner <deryni@unreliablesource.net>
2015-12-14 14:07:40 -0500

    Pull case statement out into a function.

--- tildeExpand.sh
+++ tildeExpand.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
+tildecase() {
+    case $path in
+        "~"|"~"/*)
+            path=${HOME-~}${path:1}
+            ;;
+        "~"[0-9]|"~"[+-][0-9])
+            local num=${path:1}
+            if [ "${num:0:1}" = "-" ]; then
+                ((num-=1))
+            fi
+            local opath=$path
+            path=${DIRSTACK[@]:$num:1}
+            : "${path:=$opath}"
+            ;;
+        "~+"*)
+            path=$PWD${path:2}
+            ;;
+        "~-"*)
+            path=${OLDPWD:-${path:0:2}}${path:2}
+            ;;
+        "~"*)
+            local username=${path%%/*}
+            IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "${username:1}")
+            path=${homedir:-${path%%/*}}${path#$username}
+            ;;
+    esac
+}
+
 doExpand() {
     local path
     local -a resultPathElements
 
     for path in "$@"; do
-        : "$path"
-        case $path in
-            "~"|"~"/*)
-                path=${HOME-~}${path:1}
-                ;;
-            "~"[0-9]|"~"[+-][0-9])
-                local num=${path:1}
-                if [ "${num:0:1}" = "-" ]; then
-                    ((num-=1))
-                fi
-                local opath=$path
-                path=${DIRSTACK[@]:$num:1}
-                : "${path:=$opath}"
-                ;;
-            "~+"*)
-                path=$PWD${path:2}
-                ;;
-            "~-"*)
-                path=${OLDPWD:-${path:0:2}}${path:2}
-                ;;
-            "~"*)
-                local username=${path%%/*}
-                IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "${username:1}")
-                path=${homedir:-${path%%/*}}${path#$username}
-                ;;
-        esac
+        tildecase "$path"
         resultPathElements+=( "$path" )
     done
     local result
