d105246ab83789b0e95209390332ec15401ae7b0
Etan Reisner <deryni@unreliablesource.net>
2015-12-13 23:38:51 -0500

    Reindent.

--- tildeExpand.sh
+++ tildeExpand.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
 doExpand() {
-  local path
-  local -a resultPathElements
+    local path
+    local -a resultPathElements
 
-  for path in "$@"; do
-    : "$path"
-    case $path in
-      "~"|"~"/*)
-        path=${HOME-~}${path#"~"}
-        ;;
-      "~"[0-9]|"~"[+-][0-9])
-        local num=${path#"~"}
-        if [ "${num:0:1}" = "-" ]; then
-          ((num-=1))
-        fi
-        local opath=$path
-        path=${DIRSTACK[@]:$num:1}
-        : "${path:=$opath}"
-        ;;
-      "~+"*)
-        path=$PWD${path:2}
-        ;;
-      "~-"*)
-        path=${OLDPWD:-${path:0:2}}${path:2}
-        ;;
-      "~"*)
-        local username=${path%%/*}
-        IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "${username:1}")
-        path=${homedir:-${path%%/*}}${path#$username}
-        ;;
-    esac
-    resultPathElements+=( "$path" )
-  done
-  local result
-  printf -v result '%s:' "${resultPathElements[@]}"
-  printf '%s\n' "${result%:}"
+    for path in "$@"; do
+        : "$path"
+        case $path in
+            "~"|"~"/*)
+                path=${HOME-~}${path#"~"}
+                ;;
+            "~"[0-9]|"~"[+-][0-9])
+                local num=${path#"~"}
+                if [ "${num:0:1}" = "-" ]; then
+                    ((num-=1))
+                fi
+                local opath=$path
+                path=${DIRSTACK[@]:$num:1}
+                : "${path:=$opath}"
+                ;;
+            "~+"*)
+                path=$PWD${path:2}
+                ;;
+            "~-"*)
+                path=${OLDPWD:-${path:0:2}}${path:2}
+                ;;
+            "~"*)
+                local username=${path%%/*}
+                IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "${username:1}")
+                path=${homedir:-${path%%/*}}${path#$username}
+                ;;
+        esac
+        resultPathElements+=( "$path" )
+    done
+    local result
+    printf -v result '%s:' "${resultPathElements[@]}"
+    printf '%s\n' "${result%:}"
 }
 
 expandAssign() {
-  local -a pathElements
-  IFS=: read -r -a pathElements <<<"$1"
-  : "${pathElements[@]}"
-  doExpand "${pathElements[@]}"
+    local -a pathElements
+    IFS=: read -r -a pathElements <<<"$1"
+    : "${pathElements[@]}"
+    doExpand "${pathElements[@]}"
 }
 
 expandString() {
